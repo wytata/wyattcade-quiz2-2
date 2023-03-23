c82e32a495c9b632578f9eed0b1cbde84e1c1ace
Wyatt Cade <wyattrcade@gmail.com>
2023-03-23 20:00:27 +0000

    initial commit with output looking correct

--- printvars.c
+++ printvars.c
@@ -9,5 +9,40 @@ char *custom_env[] = {"USER=Pikachu", "PATH=/tmp", NULL};
 int main(void)
 {
     // TODO
-    exit(0);
+
+    // fork two child processes
+    // TODO: PARENT BEFORE CHILD1 BEFORE 
+    printf("Running parent process PID: %d\n\n", getpid());
+
+    pid_t pid1, pid2;
+
+    pid1 = fork();
+
+    if (pid1 == 0) {
+        // Child 1
+        printf("Running child process PID: %d\n", getpid());
+        const char* file = "/home/wyatt/bin/echoall";
+        const char* band = "Bandicoot";
+        const char* pac = "Pacman";
+        execle(file, file, band, pac, NULL, custom_env);
+        exit(0);
+        
+    }
+
+    int status; 
+    
+    wait(&status);
+
+    pid2 = fork();
+
+    if (pid2 == 0) {
+        //Child 2
+        printf("Running child process PID: %d\n", getpid());
+        const char* file = "/home/wyatt/bin/echoall";
+        const char* spyro = "Spyro";
+        execlp(file, file, spyro, NULL);
+        exit(0);
+    }
+    
+
 }
